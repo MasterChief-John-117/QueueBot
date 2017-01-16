e0c79160490d08a2057e4281b36581c5a9fa8aa7
MasterChief-John-117 <galenguyer@outlook.com>
2017-01-16 07:15:49 -0800

    Basic bot

---
+++ MyBot.cs
@@ -0,0 +1,59 @@
+﻿//MyBot.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Timers;
+using System.Threading;
+
+namespace DiscoBot
+{
+
+    class MyBot
+    {
+        DiscordClient discord;
+        CommandService commands;
+
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = false;
+            });
+
+            commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("hello")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Hello.");
+                });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MjcwNDIzMDM2MzIxMDcxMTA0.C13qlw.r6EucqYbLrN6KpkI7ulK3KoUoQo", TokenType.Bot);
+            });
+        }
+
+        public void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Program.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBot bot = new MyBot();
+        }
+    }
+}
+
