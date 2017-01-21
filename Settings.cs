b6ccd350cc695e93d66cccb64a1212dfcc8800d2
MasterChief-John-117 <galenguyer@outlook.com>
2017-01-21 09:57:15 -0800

    Setting class

---
+++ Settings.cs
@@ -0,0 +1,14 @@
+﻿namespace QueueBot
+{
+    public class Token
+    {
+        public static string token = "MjcwNDIzMDM2MzIxMDcxMTA0.C2Sgtw.unJ_COTNl0ctf68yehFiEAOzU6Q";
+    }
+
+    public class Ids
+    {
+        public static string ownerId = "169918990313848832";
+        public static string[] whitelist = {ownerId };
+    }
+
+}
