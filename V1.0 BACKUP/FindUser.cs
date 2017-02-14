c6f79fa930bf23bbcc270945b2dd16c52edf7e8f
MasterChief-John-117 <galenguyer@outlook.com>
2017-02-13 21:24:11 -0800

    added handler for too may users found

--- V1.0 BACKUP/FindUser.cs
+++ V1.0 BACKUP/FindUser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Discord;
 using Discord.Commands;
 
@@ -13,16 +14,23 @@ namespace QueueBot
             IEnumerable<User> users = e.Message.Client.Servers.SelectMany(s => s.Users);
             int count = 0;
             string people = "";
+            Regex rgx = new Regex(name);
+            LinkedList<ulong> ids = new LinkedList<ulong>();
             foreach (User user in users)
             {
-                if (user.Name.ToLower().Contains(name.ToLower()) && user.Server == e.Message.Server)
+                if(rgx.IsMatch(user.Name.ToLower()) && !ids.Contains(user.Id) && user.Server == e.Message.Server)
                 {
                     Console.WriteLine(user.Name);
                     count++;
                     people += user.Name + " : " + user.Id.ToString() + "\n";
+                    ids.AddFirst(user.Id);
                 }
             }
-            if (count >= 1)
+            if (count > 25)
+            {
+                return $"I found `{count}` users! Please use more strict parameters.. :sweat_smile: ";
+            }
+            else if (count >= 1 && count <= 25)
             {
                 return people;
             }
@@ -37,10 +45,11 @@ namespace QueueBot
             IEnumerable<User> users = e.Message.Client.Servers.SelectMany(s => s.Users);
             int count = 0;
             string people = "";
+            Regex rgx = new Regex(name);
             LinkedList<ulong> ids = new LinkedList<ulong>();
             foreach (User user in users)
             {
-                if (user.Name.ToLower().Contains(name.ToLower()) && !ids.Contains(user.Id))
+                if(rgx.IsMatch(user.Name.ToLower()) && !ids.Contains(user.Id))
                 {
                     Console.WriteLine(user.Name);
                     count++;
@@ -48,7 +57,11 @@ namespace QueueBot
                     ids.AddFirst(user.Id);
                 }
             }
-            if (count >= 1)
+            if (count > 25)
+            {
+                return $"I found `{count}` users! Please use more strict parameters.. :sweat_smile: ";
+            }
+            else if (count >= 1 && count <= 25)
             {
                 return people;
             }
